f8a5edf9353d3baf4ebf61107e5bbc9f42bcd896
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-10-05 09:34:18 +0900

    ユーザー編集機能の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def edit
+  end
+
+  def update
+    # user = User.find(params[:id])
+    # user.update(user_params)
+    if current_user.update(user_params)
+      # redirect_to("/")
+      redirect_to root_path
+    else
+      # render("users/edit")
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
