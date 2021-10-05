7b67cb4a27621e008a2e801d42f7ec3ca528ce50
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-10-05 10:42:05 +0900

    新規作成画面の表示

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,14 @@
+class RoomsController < ApplicationController
+  def new
+    @room = Room.new
+  end
+
+  def create
+    Room.create(room_params)
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name)
+  end
+end
